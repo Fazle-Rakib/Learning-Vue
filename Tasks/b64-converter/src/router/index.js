03255f1f891d271d55fc72a02025937f856f16b1
Pathao Ltd <fazle30@student.sust.edu>
2021-11-16 18:43:40 +0600

    Updated routing module

--- Tasks/b64-converter/src/router/index.js
+++ Tasks/b64-converter/src/router/index.js
@@ -1,16 +1,28 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Form from "../components/b64Converter.vue";
+import Home from "../components/Home.vue";
+import b64Converter from "../components/b64Converter.vue";
 import SshKeyGen from "../components/SshKeyGen.vue";
+import HashingText from "../components/HashingText";
 const routes = [
   {
     path: "/",
-    name: "Form",
-    component: Form,
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/b64Converter",
+    name: "b64Converter",
+    component: b64Converter,
   },
   {
     path: "/SshKeyGen",
     name: "SshKeyGen",
     component: SshKeyGen,
+  },
+  {
+    path: "/HashingText",
+    name: "Hashing String",
+    component: HashingText,
   },
 ];
 
